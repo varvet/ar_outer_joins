c15ee7336a0aad7f32799b23b838069fd5c12579
Jonas Nicklas <jonas.nicklas@gmail.com>
2012-11-01 17:02:01 +0100

    Refactor

--- lib/ar_outer_join.rb
+++ lib/ar_outer_join.rb
@@ -4,44 +4,59 @@ require "ar_outer_join/version"
 module ArOuterJoin
   class OuterJoinError < StandardError; end
 
-  def self.construct_join(association)
-    table = association.active_record.arel_table
-    primary_key = association.active_record.primary_key
-    joined_table = association.klass.arel_table
+  class Join
+    attr_reader :klass
 
-    case association.macro
-    when :belongs_to
-      on = Arel::Nodes::On.new(table[association.foreign_key].eq(joined_table[primary_key]))
-      [Arel::Nodes::OuterJoin.new(joined_table, on)]
-    when :has_and_belongs_to_many
-      join_model_table = Arel::Table.new(association.options[:join_table])
-      joined_primary_key = association.klass.primary_key
+    def initialize(klass)
+      @klass = klass
+    end
 
-      on1 = Arel::Nodes::On.new(join_model_table[association.foreign_key].eq(table[primary_key]))
-      on2 = Arel::Nodes::On.new(join_model_table[association.association_foreign_key].eq(joined_table[joined_primary_key]))
+    def apply(*args)
+      return klass if args.compact.blank?
 
-      [Arel::Nodes::OuterJoin.new(join_model_table, on1), Arel::Nodes::OuterJoin.new(joined_table, on2)]
-    when :has_many, :has_one
-      on = Arel::Nodes::On.new(joined_table[association.foreign_key].eq(table[primary_key]))
-      [Arel::Nodes::OuterJoin.new(joined_table, on)]
-    else
-      raise OuterJoinError, "don't know what to do with #{association.macro} association"
+      args.inject(klass) do |scope, arg|
+        association = klass.reflect_on_association(arg)
+
+        if association.is_a? ActiveRecord::Reflection::ThroughReflection
+          scope.joins(*construct_join(association.through_reflection)).
+                joins(*construct_join(association.source_reflection))
+        else
+          scope.joins(*construct_join(association))
+        end
+      end
+    end
+
+  private
+
+    def construct_join(association)
+      table = association.active_record.arel_table
+      primary_key = association.active_record.primary_key
+      joined_table = association.klass.arel_table
+
+      case association.macro
+      when :belongs_to
+        on = Arel::Nodes::On.new(table[association.foreign_key].eq(joined_table[primary_key]))
+        [Arel::Nodes::OuterJoin.new(joined_table, on)]
+      when :has_and_belongs_to_many
+        join_model_table = Arel::Table.new(association.options[:join_table])
+        joined_primary_key = association.klass.primary_key
+
+        on1 = Arel::Nodes::On.new(join_model_table[association.foreign_key].eq(table[primary_key]))
+        on2 = Arel::Nodes::On.new(join_model_table[association.association_foreign_key].eq(joined_table[joined_primary_key]))
+
+        [Arel::Nodes::OuterJoin.new(join_model_table, on1), Arel::Nodes::OuterJoin.new(joined_table, on2)]
+      when :has_many, :has_one
+        on = Arel::Nodes::On.new(joined_table[association.foreign_key].eq(table[primary_key]))
+        [Arel::Nodes::OuterJoin.new(joined_table, on)]
+      else
+        raise OuterJoinError, "don't know what to do with #{association.macro} association"
+      end
     end
   end
 
+
   def outer_join(*args)
-    return self if args.compact.blank?
-
-    args.inject(self) do |scope, arg|
-      association = reflect_on_association(arg)
-
-      if association.is_a? ActiveRecord::Reflection::ThroughReflection
-        scope.joins(*ArOuterJoin.construct_join(association.through_reflection)).
-              joins(*ArOuterJoin.construct_join(association.source_reflection))
-      else
-        scope.joins(*ArOuterJoin.construct_join(association))
-      end
-    end
+    Join.new(self).apply(*args)
   end
 end
 
